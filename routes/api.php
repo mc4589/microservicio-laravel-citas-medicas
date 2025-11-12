65bdbda05a48d0f08a12142c20b5e29bbb50a50c
Eduardo Cantuña <118578087+EduardoCantunaCela@users.noreply.github.com>
2025-11-12 01:35:57 -0500

    api.php

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,8 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
 
 // Rutas públicas
+Route::post('/registrar-usuario', [UserController::class, 'register']);
+
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
