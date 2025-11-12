3d998b6eacfc68659b09a78916d26b33a90709d1
Eduardo Cantuña <118578087+EduardoCantunaCela@users.noreply.github.com>
2025-11-12 01:35:38 -0500

    api.php

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
 
+// Rutas públicas
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
