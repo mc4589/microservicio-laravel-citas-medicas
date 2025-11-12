fc81b723a28a300876d7cc907cb64b998a803639
Eduardo Cantuña <118578087+EduardoCantunaCela@users.noreply.github.com>
2025-11-12 01:35:17 -0500

    api.php

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\PerfilController;
+use App\Http\Controllers\UserController;
+
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
